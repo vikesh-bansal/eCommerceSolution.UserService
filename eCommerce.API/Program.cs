857666a84126b3ccba308ea1374c34e62e543fbd
vikesh-bansal <vikesh.bansal1@gmail.com>
2025-09-28 20:00:13 +0530

    Added Swagger, Cors and JsonSerializer conter for Enum

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -16,10 +16,19 @@ builder.Services.AddCore();//Add Core services
 
 
 //Add controllers to the service collection
-builder.Services.AddControllers().AddJsonOptions(options=> options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly); // atleast one datamapper is required, as it inherited from profile so rest are automatically mapped by system.
- 
+
+//Add swagger generation services to create swagger specificication
+builder.Services.AddSwaggerGen();
+
+//Add cors Services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod());
+});
+
 // Build the web application
 var app = builder.Build();
 //Add Exception Handling middleware
@@ -27,6 +36,10 @@ app.UseExceptionHandlingMiddleware();
 
 //Routing
 app.UseRouting();
+app.UseSwagger(); // Adds endpoint that can server the swagger.json
+app.UseSwaggerUI();// Adds swagger UI (interactive page to explore the test API endpoints)
+
+app.UseCors(); // Allow application with different domain can use this API or we enable this for Angular
 
 //Auth
 app.UseAuthentication();
